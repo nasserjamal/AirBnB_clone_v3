5484b309076d3ffd0073c452b08bd544a07ed9fe
Nasser Jamal <nasserjamal30@gmail.com>
2023-01-22 11:42:05 +0300

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,31 @@
 """Ata hii sijui ni nini"""
 
 from api.v1.views import app_views, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+stats_data =\
+    {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
 
 
 @app_views.route("/status")
 def index():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    return jsonify(stats_data)
