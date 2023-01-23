e0a52331f215b90a92e49dde09091b666cbb97cc
Nasser Jamal <nasserjamal30@gmail.com>
2023-01-23 10:49:37 +0300

    trying alternative (anne)

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -28,7 +28,32 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats/", methods=["GET"], strict_slashes=False)
+@app_views.route('/stats/')
 def stats():
-    """Returns the number of each of the classes in the database"""
-    return jsonify(stats_data)
+    """Example endpoint returns stats
+    returns a number of objects of each class
+    ---
+    definitions:
+      status:
+        type: object
+      Color:
+        type: string
+      items:
+        $ref: '#/definitions/Color'
+    responses:
+      200:
+        description: dictionary with 'status' as key and 'ok' as keyvalue
+        schema:
+          $ref: '#/definitions/State'
+        examples:
+           { "amenities": 47, "cities": 36, "places": 154, "reviews": 718,
+             "states": 27, "users": 31}
+    """
+    models_available = {"User": "users",
+                        "Amenity": "amenities", "City": "cities",
+                        "Place": "places", "Review": "reviews",
+                        "State": "states"}
+    stats = {}
+    for cls in models_available.keys():
+        stats[models_available[cls]] = storage.count(cls)
+    return jsonify(stats)
