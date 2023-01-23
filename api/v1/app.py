3b64b17336375d25782534a854b1e451d7f15fed
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-01-23 10:25:02 +0300

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,18 @@
 
 
 from os import getenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error handler taking care of 404 error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 @app.teardown_appcontext
 def on_app_close(ja3):
@@ -20,5 +25,4 @@ def on_app_close(ja3):
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
-    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
